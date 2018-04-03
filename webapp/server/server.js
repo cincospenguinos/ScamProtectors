9e03f7bdb8fd38a2b4ce6b33a5e65fd1c14a03a3
Jessica Larsen <jesslarsen21@gmail.com>
2018-04-03 11:17:21 -0600

    Added simple server and tiny api

---
+++ webapp/server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+app.get('/help', function (req, res) {
+  res.send('You did it!')
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
