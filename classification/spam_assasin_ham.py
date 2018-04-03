b045882a7c806b5aaa895ba95fa7f333426990f7
Andre LaFleur <andre.lafleur@utah.edu>
2018-04-03 09:23:09 -0600

    Grab spam assasin ham emails

--- classification/spam_assasin_ham.py
+++ classification/spam_assasin_ham.py
@@ -6,11 +6,19 @@ Gather up all of the spam assasin ham emails.
 import os
 from email import *
 
+'''
+get_all_emails()
+
+Returns all ham emails from spam assasin.
+
+TODO: I'm getting weird errors on ubuntu, so I had it ignore the errors. Down the road
+we will want to remove that.
+'''
 def get_all_emails():
 	collection = []
 	names = os.listdir('dataset/spam_assasin_ham')
 	for name in names:
-		with open('dataset/spam_assasin_ham/' + name, 'r', encoding='ascii') as f:
+		with open('dataset/spam_assasin_ham/' + name, 'r', errors='ignore') as f:
 			text = f.read()
 			mail = Email(text, 0)
 			collection.append(mail)
@@ -18,7 +26,9 @@ def get_all_emails():
 	return collection
 
 def main():
-	get_all_emails()
+	emails = get_all_emails()
+	for mail in emails:
+		print(mail.text)
 
 if __name__ == "__main__":
 	main()
